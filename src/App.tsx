0c0518b07c395f9a664f9e1cb5654f553ce3897c
Alex <alexandr.webwork@gmail.com>
2022-12-25 20:13:21 +0200

    contains minimum features

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import VideoPlayer from './components/VideoPlayer';
+import VideoPlayer from "./components/VideoPlayer";
 
 import video1 from "./videos/1.mp4";
 import video2 from "./videos/2.mp4";
@@ -7,69 +7,73 @@ import video3 from "./videos/3.mp4";
 function App() {
   const videos = [
     {
-        title: 'Title 1', 
-        url: video1
+      title: "Title 1",
+      url: video1,
     },
     {
-        title: 'Title 2', 
-        url: video2
+      title: "Title 2",
+      url: video2,
     },
     {
-        title: 'Title 3', 
-        url: video3
-    }
+      title: "Title 3",
+      url: video3,
+    },
   ];
 
   const events = [
     {
-      title: 'Event 1',
+      title: "Event 1",
+      videoTime: 3,
+      videoUrl: video1,
+    },
+    {
+      title: "Event 2",
       videoTime: 5,
-      videoUrl: video1
+      videoUrl: video1,
     },
     {
-      title: 'Event 2',
-      videoTime: 10,
-      videoUrl: video1
+      title: "Event 3",
+      videoTime: 7,
+      videoUrl: video1,
     },
     {
-      title: 'Event 3',
-      videoTime: 15,
-      videoUrl: video1
-    },
-    {
-      title: 'Event 1',
+      title: "Event 4",
       videoTime: 5,
-      videoUrl: video2
+      videoUrl: video2,
     },
     {
-      title: 'Event 2',
+      title: "Event 5",
       videoTime: 10,
-      videoUrl: video2
+      videoUrl: video2,
     },
     {
-      title: 'Event 3',
+      title: "Event 6",
       videoTime: 15,
-      videoUrl: video2
+      videoUrl: video2,
     },
     {
-      title: 'Event 1',
-      videoTime: 5,
-      videoUrl: video3
+      title: "Event 7",
+      videoTime: 2,
+      videoUrl: video3,
     },
     {
-      title: 'Event 2',
-      videoTime: 10,
-      videoUrl: video3
+      title: "Event 8",
+      videoTime: 4,
+      videoUrl: video3,
     },
     {
-      title: 'Event 3',
-      videoTime: 15,
-      videoUrl: video3
+      title: "Event 9",
+      videoTime: 6,
+      videoUrl: video3,
     },
   ];
 
   return (
-    <VideoPlayer videos={videos} events={events}/>
+    <>
+      <div className="container">
+        <VideoPlayer videos={videos} events={events} />
+      </div>
+    </>
   );
 }
 
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,52 +1,215 @@
-import './VideoPlayer.css';
+import "./VideoPlayer.scss";
 
-import { useState } from 'react';
+import { useState, useRef, useEffect } from "react";
+
+interface Video {
+  title: string;
+  url: string;
+}
 
 interface Props {
-    videos: {
-      title: string;
-      url: string;
-    }[];
-    events: {
-      title: string;
-      videoTime: number;
-      videoUrl: string;
-    }[];
-  }
-   
-  function VideoPlayer({ videos, events }: Props): JSX.Element {
-  const [title, setTitle] = useState(videos[0].title);
+  videos: Video[];
+  events: {
+    title: string;
+    videoTime: number;
+    videoUrl: string;
+  }[];
+}
 
-    return (
-        <div className="video-player">
-            <div className="video-player__main">
-            <span className='video-player__title'>{title}</span>
-            <select 
+const VideoPlayer = ({ videos, events }: Props) => {
+  const videoEl = useRef<HTMLVideoElement>(null);
+  const video = videoEl.current;
+  const [videoPlaying, setVideoPlaying] = useState<boolean>(false);
+  const [videoCanPlay, setVideoCanPlay] = useState<boolean>(false);
+
+  const [currentVideo, setCurrentVideo] = useState<Video>(videos[0]);
+  const [currentVideoDuration, setCurrentVideoDuration] = useState<number>(0);
+  const [currentVideoTime, setCurrentVideoTime] = useState<number>(0);
+
+  const seekValue = 5;
+
+  const filterdEvents = events.filter(
+    (item) => item.videoUrl === currentVideo.url
+  );
+
+  const [currentEvents, setCurrentEvents] = useState<{}[]>(filterdEvents);
+
+  const onChangeHandle = (e: { target: HTMLSelectElement }) => {
+    // setVideoPlaying(false);
+    const current = videos.find((obj) => obj.title === e.target.value);
+    if (current) {
+      setCurrentVideo(current);
+    }
+  };
+
+  const eventButtons = document.querySelectorAll(".vp__event");
+
+  const onTimeUpdateHandle = (e: any) => {
+    const currentTime = e.target.currentTime;
+    setCurrentVideoTime(currentTime);
+    eventButtons.forEach((button) => {
+      const eventTime = Number(button.getAttribute("data-time"));
+      if (currentTime >= eventTime) {
+        button.classList.add("vp__event--current");
+      } else {
+        button.classList.remove("vp__event--current");
+      }
+    });
+  };
+
+  const videoPlayingHandle = () => {
+    if (video) {
+      if (videoPlaying) {
+        setVideoPlaying(false);
+        video.pause();
+        return;
+      }
+      video.play();
+      setVideoPlaying(true);
+    }
+  };
+
+  const loadedMetadataHandle = (e: any) => {
+    setCurrentVideoTime(e.target.currentTime);
+  };
+
+  const onLoadedDataHandle = (e: any) => {
+    setCurrentVideoDuration(e.target.duration);
+    if (!video) return;
+    setCurrentEvents(filterdEvents);
+  };
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className="vp">
+      <div className="vp__main">
+        <div className="vp__header">
+          <h1 className="vp__title">{currentVideo.title}</h1>
+          <select
+            className="vp__select"
+            value={currentVideo.title}
             onChange={(e) => {
-                setTitle(e.target.value)
-            }} name="" id="">
-                {videos.map((video, i) => (
-                    <option key={i} value={video.title}>{video.title}</option>
-                    ))}
-            </select>
-            <video className="video-player__video" src={videos[0].url}></video>
-            <button>-5</button>
-            <button>Play</button>
-            <button>+5</button>
-            <input type="range" />
-            </div>
-            <div className="video-player__sidebar">
-            <ul>
-            {events.map((event, i) => (
-                    <li key={i}>
-                        <button type='button'>{event.title}</button>
-                    </li>
-                    ))}
-               
-            </ul>
-            </div>
+              onChangeHandle(e);
+            }}
+          >
+            {videos.map((video, i) => (
+              <option key={i} value={video.title}>
+                {video.title}
+              </option>
+            ))}
+          </select>
         </div>
-    );
-  }
+        <video
+          ref={videoEl}
+          className="vp__video"
+          src={currentVideo.url}
+          onLoadedMetadata={(e) => {
+            loadedMetadataHandle(e);
+          }}
+          onTimeUpdate={(e) => {
+            onTimeUpdateHandle(e);
+          }}
+          onPause={() => {
+            setVideoPlaying(false);
+          }}
+          onPlaying={() => {
+            setVideoPlaying(true);
+          }}
+          onCanPlay={(e) => {
+            setVideoCanPlay(true);
+            if (videoPlaying) {
+              video?.play();
+            }
+          }}
+          onLoadedData={(e) => {
+            onLoadedDataHandle(e);
+          }}
+        ></video>
+        <div className="vp__controls">
+          <button
+            className="vp__button"
+            type="button"
+            disabled={videoCanPlay ? false : true}
+            onClick={() => {
+              if (video) {
+                if (video.currentTime <= seekValue) {
+                  video.currentTime = 0;
+                } else {
+                  video.currentTime -= seekValue;
+                }
+              }
+            }}
+          >
+            {"-" + seekValue}
+          </button>
+          <button
+            className="vp__button vp__button--play"
+            type="button"
+            disabled={videoCanPlay ? false : true}
+            onClick={() => {
+              videoPlayingHandle();
+            }}
+          >
+            {videoPlaying ? "Pause" : "Play"}
+          </button>
+          <button
+            className="vp__button"
+            type="button"
+            disabled={videoCanPlay ? false : true}
+            onClick={() => {
+              if (video) {
+                if (video.currentTime + seekValue > video.duration) {
+                  video.currentTime = video.duration;
+                } else {
+                  video.currentTime += seekValue;
+                }
+              }
+            }}
+          >
+            {"+" + seekValue}
+          </button>
+          <input
+            type="range"
+            min="0"
+            step="0.01"
+            value={currentVideoTime}
+            max={currentVideoDuration}
+            onChange={(e: { target: HTMLInputElement }) => {
+              if (video) {
+                video.currentTime = Number(e.target.value);
+              }
+            }}
+            className="vp__progress-bar"
+          />
+        </div>
+      </div>
+      <div className="vp__sidebar">
+        <ul className="vp__events">
+          {currentEvents.map((event: any, i: number) => (
+            <li className="vp__event-item" key={i}>
+              <button
+                data-time={event.videoTime}
+                className="vp__event"
+                type="button"
+                onClick={(e) => {
+                  if (video) {
+                    const time = e.currentTarget.getAttribute("data-time");
+                    video.currentTime = Number(time);
+                    if (videoPlaying) {
+                      video.play();
+                    }
+                  }
+                }}
+              >
+                {event.title}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-  export default VideoPlayer;
+export default VideoPlayer;
